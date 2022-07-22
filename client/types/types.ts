77f3ab8fa0bee7b6696902490e77d2e4127cf3d5
Mikhail Ivanovskii <mischaiv19@gmail.com>
2022-07-22 22:18:05 +0200

    feat: added product type

--- client/types/types.ts
+++ client/types/types.ts
@@ -1,9 +1,45 @@
+// CONTACT FORM TYPE
+
 export interface IContactForm {
     name: string;
     email: string;
     phone: string;
-    business: string;
     message: string;
+    isSendCopyEnabled: boolean;
 }
 
-export interface API {}
+// PRODUCT TYPE
+
+interface IColor {
+    name: string;
+    imageSrc: string;
+    imageAlt: string;
+    class: string;
+    selectedClass: string;
+}
+
+interface ISize {
+    name: string;
+    inStock: boolean;
+}
+
+interface IProduct {
+    pid: number;
+    name: string;
+    href: string;
+    price: string;
+    rating: number;
+    reviewCount: number;
+    colors: IColor[];
+    sizes: ISize[];
+}
+
+export interface IManProduct {
+    id: number;
+    product: IProduct;
+}
+
+export interface IWomanProduct {
+    id: number;
+    product: IProduct;
+}
