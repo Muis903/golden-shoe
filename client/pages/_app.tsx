2777bad4a7a0faf2755e3ab5226d55867fb1bf30
Mikhail Ivanovskii <mischaiv19@gmail.com>
2022-07-22 22:04:51 +0200

    feat: added components such as Footer, Navbar, Modal...

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { NextUIProvider } from "@nextui-org/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            <NextUIProvider>
+                <Navbar />
+                <Component {...pageProps} />
+                <Toaster />
+                <Footer />
+            </NextUIProvider>
+        </div>
+    );
 }
 
-export default MyApp
+export default MyApp;
