c456487cae071d2d13e6a06de88d6718787dfcf8
Mikhail Ivanovskii <mischaiv19@gmail.com>
2022-07-22 22:29:46 +0200

    feat: added product/ and smtp/ urls

--- server/server/server/urls.py
+++ server/server/server/urls.py
@@ -14,6 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("smtp.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("smtp/", include("smtp.urls")),
+    path("product/", include("product.urls")),
+]
