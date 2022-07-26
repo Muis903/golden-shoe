fd602a4c60b6f37ef4f834e411b1875ec98309e9
Mikhail Ivanovskii <mischaiv19@gmail.com>
2022-07-26 14:02:17 +0200

    style: added comments and removed some types

--- client/types/types.ts
+++ client/types/types.ts
@@ -1,45 +1,43 @@
 // CONTACT FORM TYPE
 
+// NOTE: If you change any name of the key than the API call will fail!
 export interface IContactForm {
-    name: string;
-    email: string;
-    phone: string;
-    message: string;
-    isSendCopyEnabled: boolean;
+    name: string; // name of user
+    email: string; // email of user
+    phone: string; // phone of user
+    message: string; // message of user
+    isSendCopyEnabled: boolean; // whether send or not copy of message to user
 }
 
 // PRODUCT TYPE
 
+// NOTE: If you change any name of the key than the API call will fail!
 interface IColor {
-    name: string;
-    imageSrc: string;
-    imageAlt: string;
-    class: string;
-    selectedClass: string;
+    name: string; // name of the color (white)
+    imageSrc: string; // image URL (https://.....png...)
+    imageAlt: string; // image Alternative name (This are white shoes)
+    class: string; // tailwind CSS color when not selected
+    selectedClass: string; // tailwind CSS color when selected
 }
 
+// NOTE: If you change any name of the key than the API call will fail!
 interface ISize {
-    name: string;
-    inStock: boolean;
+    name: string; // size name for a shoe (42.5)
+    inStock: boolean; // whether the shoe in stock or not (true/false). If not in stock user will be not able to choose it to buy.
 }
 
-interface IProduct {
-    pid: number;
-    name: string;
-    href: string;
-    price: string;
-    rating: number;
-    reviewCount: number;
-    colors: IColor[];
-    sizes: ISize[];
-}
-
-export interface IManProduct {
-    id: number;
-    product: IProduct;
-}
-
-export interface IWomanProduct {
-    id: number;
-    product: IProduct;
+// NOTE: If you change any name of the key than the API call will fail!
+export interface IProduct {
+    id: number; // primary key id that is automaticly defined by Django while saving database
+    product: {
+        // product object wherein all props are locating
+        pid: number; // product id that we defining in backend
+        name: string; // name of product
+        href: string; // URL to product
+        price: string; // price of product in British pounds
+        rating: number; // user rating of shoe that we self defined on backend for now
+        reviewCount: number; // how many reviews are there that made by users how has purchased the shoes
+        colors: IColor[]; // colors of the shoe model
+        sizes: ISize[]; // sizes of the shoe
+    };
 }
