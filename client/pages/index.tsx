9e806d7401ca45b10de41148607170e6475ccefd
Mikhail Ivanovskii <mischaiv19@gmail.com>
2022-07-26 14:01:26 +0200

    fix: made Modal work properly, implemented women shoes

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,85 +1,129 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useEffect, useState, Fragment } from "react";
-import { IManProduct, IWomanProduct } from "../types/types";
+import { IProduct } from "../types/types";
 import { getMenProducts, getWomenProducts } from "../helpers/api";
 
 import { Dialog, RadioGroup, Transition } from "@headlessui/react";
 import { XIcon } from "@heroicons/react/outline";
 import { StarIcon } from "@heroicons/react/solid";
 
-const product = {
-    name: "Basic Tee 6-Pack ",
-    price: "$192",
-    rating: 3.9,
-    reviewCount: 117,
-    href: "#",
-    imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/product-quick-preview-02-detail.jpg",
-    imageAlt: "Two each of gray, white, and black shirts arranged on table.",
-    colors: [
-        {
-            name: "White",
-            imgSrc: "",
-            class: "bg-white",
-            selectedClass: "ring-gray-400",
-        },
-        {
-            name: "Gray",
-            imgSrc: "",
-            class: "bg-gray-200",
-            selectedClass: "ring-gray-400",
-        },
-        {
-            name: "Black",
-            imgSrc: "",
-            class: "bg-gray-900",
-            selectedClass: "ring-gray-900",
-        },
-    ],
-    sizes: [
-        { name: "XXS", inStock: true },
-        { name: "XS", inStock: true },
-        { name: "S", inStock: true },
-        { name: "M", inStock: true },
-        { name: "L", inStock: true },
-        { name: "XL", inStock: true },
-        { name: "XXL", inStock: true },
-        { name: "XXXL", inStock: false },
-    ],
-};
-
+// This function came with Tailwind CSS template so we place it here.
 function classNames(...classes: any[]) {
     return classes.filter(Boolean).join(" ");
 }
 
-const Home: NextPage = () => {
-    const [menProducts, setMenProducts] = useState<IManProduct[] | undefined>();
-    const [womenProducts, setWomenProducts] = useState<
-        IWomanProduct[] | undefined
-    >();
-    const [showModal, setShowModal] = useState<boolean>(false);
-    const [selectedColor, setSelectedColor] = useState(product.colors[0]);
-    const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
-    const [pid, setPid] = useState<number>();
-    const [cart, setCart] = useState<number>();
+// Using this template we can avoid an error that has relation to undefined.
+// This error is partly specific to TypeScript to prevent future errors in production.
+let defaultProduct: IProduct[] = [
+    {
+        id: 0,
+        product: {
+            pid: 0,
+            name: "",
+            href: "",
+            price: "",
+            rating: 0,
+            reviewCount: 0,
+            colors: [
+                {
+                    name: "",
+                    imageSrc: "",
+                    imageAlt: "",
+                    class: "",
+                    selectedClass: "",
+                },
+            ],
+            sizes: [{ name: "", inStock: false }],
+        },
+    },
+];
 
+/**
+ * Home page for Golden Show.
+ *
+ * Here user can interact with assortiment, choose a desired shoe and add it to cart to purchase it later.
+ */
+const Home: NextPage = () => {
+    // Initialize men products as an array of IProduct type and assign default product.
+    // This default product will be erased once we have fetched all women products from backend.
+    const [menProducts, setMenProducts] = useState<IProduct[]>(defaultProduct);
+
+    // Initialize women products as an array of IProduct type and assign default product.
+    // This default product will be erased once we have fetched all women products from backend.
+    const [womenProducts, setWomenProducts] = useState<IProduct[] | undefined>(
+        defaultProduct
+    );
+
+    // Initialize show modal variable that is boolean. This variable has a mission
+    // to show modal with the product description as well as with add to cart button.
+    const [showModal, setShowModal] = useState<boolean>(false);
+
+    // Initialize variable for selected color that is used by Tailwind CSS.
+    // This variable is required and came with Tailwind CSS template.
+    const [selectedColor, setSelectedColor] = useState();
+
+    // Initialize variable for selected size that is used by Tailwind CSS.
+    // This variable is required and came with Tailwind CSS template.
+    const [selectedSize, setSelectedSize] = useState();
+
+    // Initialize variable that saves a product (man's or woman's) by clicking a product card.
+    // This variable we use to push a chozen product as IProduct object into the defined cart array.
+    const [chozenProduct, setChozenProduct] = useState<IProduct>(
+        defaultProduct[0]
+    );
+
+    // Initialize variable wherein all chozen products are saved to show all of them in cart section.
+    // This variable is used to show all products that user has chozen to buy. So then it all will be
+    // shown in Cart.
+    let cart: IProduct[] = defaultProduct;
+
+    // Initialize variable in order to control API workflow when we fetching (GET) all products from backend.
+    // If do not initialize this variable then we will received infinite API (GET) calls which is not good.
+    // When we have finished to fetch products the `fetchedProducts` variable will be set as true so we
+    // prevent the above scenario.
+    const [fetchedProducts, setFetchedProducts] = useState<boolean>(false);
+
+    /**
+     * Fetch all products in the form of JSON from backend as GET request.
+     * Then save the products separately to each variable.
+     */
     async function getProducts() {
+        // Fetch. Making API call to backend.
         const menProducts = await getMenProducts();
         const womenProducts = await getWomenProducts();
 
+        // Save it.
         setMenProducts(menProducts);
         setWomenProducts(womenProducts);
     }
 
+    /**
+     * Add a chozen product to the cart.
+     * This function is beeing called when user clicks add to cart button in the modal.
+     */
+    function addProductToCart(product: IProduct) {
+        // If product is not undefined then push it into the cart array.
+        if (product !== undefined) {
+            cart.push(product);
+        }
+
+        // For testing console log. Not required to run this log.
+        console.log(cart);
+    }
+
     // If page is refreshed we calling API to fetch products from backend.
     useEffect(() => {
-        // menProducts array must be empty otherwise we will call for always.
-        if (!menProducts) {
+        // Value of fetchedProducts variable must be false (as by default).
+        if (fetchedProducts === false) {
+            // Make API call to backend to get products only once.
             getProducts();
+            // Set variable to true so we prevent infinite (unstoppable) calls.
+            setFetchedProducts(true);
         }
-    });
+    }, [fetchedProducts]); // Effect will only activate if the fetchedProducts value in the list change.
 
+    // Return UI.
     return (
         <div>
             <Head>
@@ -173,11 +217,14 @@ const Home: NextPage = () => {
                         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                             {menProducts.map((product) => (
                                 <a
-                                    key={product.product.pid}
+                                    key={product.id}
                                     href={product.product.href}
                                     className="group"
                                     type="button"
-                                    onClick={() => setShowModal(true)}
+                                    onClick={() => {
+                                        setShowModal(true);
+                                        setChozenProduct(product);
+                                    }}
                                 >
                                     <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
                                         <img
@@ -221,9 +268,13 @@ const Home: NextPage = () => {
                         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                             {womenProducts.map((product) => (
                                 <a
-                                    key={product.product.pid}
+                                    key={product.id}
                                     href={product.product.href}
                                     className="group"
+                                    onClick={() => {
+                                        setShowModal(true);
+                                        setChozenProduct(product);
+                                    }}
                                 >
                                     <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
                                         <img
@@ -305,14 +356,27 @@ const Home: NextPage = () => {
                                             <div className="w-full grid grid-cols-1 gap-y-8 gap-x-6 items-start sm:grid-cols-12 lg:gap-x-8">
                                                 <div className="aspect-w-2 aspect-h-3 rounded-lg bg-gray-100 overflow-hidden sm:col-span-4 lg:col-span-5">
                                                     <img
-                                                        src={product.imageSrc}
-                                                        alt={product.imageAlt}
+                                                        src={
+                                                            chozenProduct
+                                                                .product
+                                                                .colors[0]
+                                                                .imageSrc
+                                                        }
+                                                        alt={
+                                                            chozenProduct
+                                                                .product
+                                                                .colors[0]
+                                                                .imageAlt
+                                                        }
                                                         className="object-center object-cover"
                                                     />
                                                 </div>
                                                 <div className="sm:col-span-8 lg:col-span-7">
                                                     <h2 className="text-2xl font-extrabold text-gray-900 sm:pr-12">
-                                                        {product.name}
+                                                        {
+                                                            chozenProduct
+                                                                .product.name
+                                                        }
                                                     </h2>
 
                                                     <section
@@ -327,7 +391,11 @@ const Home: NextPage = () => {
                                                         </h3>
 
                                                         <p className="text-2xl text-gray-900">
-                                                            {product.price}
+                                                            {
+                                                                chozenProduct
+                                                                    .product
+                                                                    .price
+                                                            }
                                                         </p>
 
                                                         {/* Reviews */}
@@ -349,7 +417,9 @@ const Home: NextPage = () => {
                                                                                     rating
                                                                                 }
                                                                                 className={classNames(
-                                                                                    product.rating >
+                                                                                    chozenProduct
+                                                                                        .product
+                                                                                        .rating >
                                                                                         rating
                                                                                         ? "text-gray-900"
                                                                                         : "text-gray-200",
@@ -362,17 +432,21 @@ const Home: NextPage = () => {
                                                                 </div>
                                                                 <p className="sr-only">
                                                                     {
-                                                                        product.rating
+                                                                        chozenProduct
+                                                                            .product
+                                                                            .rating
                                                                     }{" "}
                                                                     out of 5
                                                                     stars
                                                                 </p>
                                                                 <a
                                                                     href="#"
-                                                                    className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500"
+                                                                    className="ml-3 text-sm font-medium text-[#d5785a] hover:text-[#fd6432]"
                                                                 >
                                                                     {
-                                                                        product.reviewCount
+                                                                        chozenProduct
+                                                                            .product
+                                                                            .reviewCount
                                                                     }{" "}
                                                                     reviews
                                                                 </a>
@@ -412,7 +486,7 @@ const Home: NextPage = () => {
                                                                         color
                                                                     </RadioGroup.Label>
                                                                     <span className="flex items-center space-x-3">
-                                                                        {product.colors.map(
+                                                                        {chozenProduct.product.colors.map(
                                                                             (
                                                                                 color
                                                                             ) => (
@@ -471,7 +545,7 @@ const Home: NextPage = () => {
                                                                     </h4>
                                                                     <a
                                                                         href="#"
-                                                                        className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
+                                                                        className="text-sm font-medium text-[#d5785a] hover:text-[#fd6432]"
                                                                     >
                                                                         Size
                                                                         guide
@@ -492,7 +566,7 @@ const Home: NextPage = () => {
                                                                         size
                                                                     </RadioGroup.Label>
                                                                     <div className="grid grid-cols-4 gap-4">
-                                                                        {product.sizes.map(
+                                                                        {chozenProduct.product.sizes.map(
                                                                             (
                                                                                 size
                                                                             ) => (
@@ -582,8 +656,13 @@ const Home: NextPage = () => {
                                                             </div>
 
                                                             <button
-                                                                type="submit"
-                                                                className="mt-6 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                                                type="button"
+                                                                className="mt-6 w-full border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white bg-[#d5785a] hover:bg-[#fd6432] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                                                onClick={() =>
+                                                                    addProductToCart(
+                                                                        chozenProduct
+                                                                    )
+                                                                }
                                                             >
                                                                 Add to bag
                                                             </button>
